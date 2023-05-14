14dcf12014db9f2a8846e21d782340279f8b3935
misterdvlpr <misterdvlpr@gmail.com>
2023-05-14 20:56:33 +0700

    feat: added `Input` component

---
+++ src/components/Input.tsx
@@ -0,0 +1,40 @@
+import { FC } from "react";
+import clsx from "clsx";
+
+interface InputProps {
+  id: string;
+  label?: string;
+  placeholder?: string;
+  value?: string;
+  disabled?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input: FC<InputProps> = ({
+  id,
+  label = "Label",
+  placeholder = "",
+  value,
+  disabled = false,
+  onChange,
+}) => (
+  <div className="flex flex-col">
+    <label
+      htmlFor={id}
+      className={clsx("block text-sm font-medium", {
+        ["opacity-20"]: disabled,
+      })}
+    >
+      {label}
+    </label>
+    <input
+      name={id}
+      id={id}
+      placeholder={placeholder}
+      value={value}
+      disabled={disabled}
+      onChange={onChange}
+      className="border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  disabled:opacity-20"
+    />
+  </div>
+);
