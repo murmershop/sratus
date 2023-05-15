89e1ec08d497da7b77a5184e1683aa1fa726ef12
misterdvlpr <misterdvlpr@gmail.com>
2023-05-15 22:31:55 +0700

    fix: cleanup svg error props

--- src/svg/Checkout.tsx
+++ src/svg/Checkout.tsx
@@ -4,7 +4,7 @@ export const CheckoutIcon: FC = () => (
   <svg
     stroke="currentColor"
     fill="currentColor"
-    stroke-width="0"
+    strokeWidth="0"
     viewBox="0 0 24 24"
     height="1em"
     width="1em"
